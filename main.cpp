180a1486f349c403e627f8dd7ab54d7312d49877
The-Green-Man-12 <masongross144@gmail.com>
2026-05-28 18:33:29 -0400

    Enemy stuff

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@
 #include <ctime>
 using namespace std;
 void battle(string& encounter, int& maxHealth, int& health, int& attack, int& defense);
-void printStats(int& maxHealth, int& health, int& attack, int& defense, vector<string>& effects);
-void introductions(string& encounter);
-int printOptions();
+void printStats(int& maxHealth, int& health, int& attack, int& defense, vector<string>& effects, vector<int>& enemyStats);
+void enemyAttacks(string& encounter, int& health, int& attack, int& defense, vector<int>& enemyStats);
+vector<int> introductions(string& encounter);
+int printOptions(vector<string>& options);
 void wait(int milliseconds);
 
 
@@ -20,7 +21,8 @@ int main() {
     int health = 100;
     int attack = 20;
     int defense = 0;
-    int randomNum = rand() % possibleEncounters.size();
+    //int randomNum = rand() % possibleEncounters.size();
+    int randomNum = 0;
     string encounter = possibleEncounters[randomNum];
     cout << "You encountered a " << encounter << "!" << endl;
     wait(1000);
@@ -29,44 +31,79 @@ int main() {
 }
 
 void battle(string& encounter, int& maxHealth, int& health, int& attack, int& defense) {
+    bool dead = false;
     vector<string> currentEffects;
+    vector<string> options = {"Attack", "Act", "Item"};
     int choice;
+    int damage;
     cout << "Battle starts..." << endl;
     wait(1000);
     if (encounter == "Table") {
-        introductions(encounter);
-        printStats(maxHealth, health, attack, defense, currentEffects);
-        choice = printOptions();
+        vector<int> enemyStats = introductions(encounter);
+        vector<string> attackOptions = {"Hit the table with your fist", "Kick the table"};
+        do {
+            printStats(maxHealth, health, attack, defense, currentEffects, enemyStats);
+            choice = printOptions(options);
+            if(choice == 1) {
+                cout << "How do you want to attack the table?" << endl;
+                int attack = printOptions(attackOptions);
+                if(attack == 1) {
+                    damage = 10;
+                    enemyStats[0] -= damage;
+                    cout << "You hit the table with your fist!" << endl;
+                    wait(1000);
+                    cout << "You deal " << damage << " damage to the table." << endl;
+                    wait(1000);
+                } else if(attack == 2) {
+                    health -= 3;
+                    damage = 5;
+                    enemyStats[0] -= damage;
+                    cout << "You kick the table!" << endl;
+                    wait(1000);
+                    cout << "You deal " << damage << " damage to the table." << endl;
+                    wait(1000);
+                    cout << "But you also stub your toe :(" << endl;
+                    wait(1000);
+                } else {
+                    cout << "Invalid attack choice." << endl;
+                    wait(1000);
+                }
+                enemyAttacks(encounter, health, attack, defense, enemyStats);
+            }
+        } while(dead == false);
 
     } else if (encounter == "Spider man") {
+        vector<int> enemyStats = introductions(encounter);
         introductions(encounter);
-        printStats(maxHealth, health, attack, defense, currentEffects);
-        choice = printOptions();
+        printStats(maxHealth, health, attack, defense, currentEffects, enemyStats);
+        choice = printOptions(options);
     } else if (encounter == "The Green Man") {
+        vector<int> enemyStats = introductions(encounter);
         introductions(encounter);
-        printStats(maxHealth, health, attack, defense, currentEffects);
-        choice = printOptions();
+        printStats(maxHealth, health, attack, defense, currentEffects, enemyStats);
+        choice = printOptions(options);
     } else if (encounter == "Knife") {
+        vector<int> enemyStats = introductions(encounter);
         introductions(encounter);
-        printStats(maxHealth, health, attack, defense, currentEffects);
-        choice = printOptions();
+        printStats(maxHealth, health, attack, defense, currentEffects, enemyStats);
+        choice = printOptions(options);
     } else {
         cout << "Error: Encounter not recognized." << endl;
     }
 }
 
-int printOptions() {
+int printOptions(vector<string>& options) {
     string input;
     cout << "Choose your action: " << endl;
-    cout << "1. Attack" << endl;
-    cout << "2. Act" << endl;
-    cout << "3. Item" << endl;
+    for (int i = 0; i < options.size(); i++) {
+        cout << i + 1 << ". " << options[i] << endl;
+    }
     try {
         if (!getline(cin, input) || input.empty() || input == "\n") {
             throw invalid_argument("Invalid input");
         }
         int choice = stoi(input);
-        if (choice < 1 || choice > 3) {
+        if (choice < 1 || choice > options.size()) {
             throw out_of_range("Value out of range");
         }
         return choice;
@@ -81,13 +118,70 @@ int printOptions() {
     }
 }
 
-void introductions(string& encounter) {
-    if(encounter == "Table") {
-        cout << "the ordinary table wasn't ordinary because table was not table" << endl;
-        wait(1000);
-        cout << "table" << endl;
-        wait(1000);
+void enemyAttacks(string& encounter, int& health, int& attack, int& defense, vector<int>& enemyStats) {
+
+    if (encounter == "Table") {
+        vector<string> possibleAttacks = {"The table hits you with its leg!", "The table hits you with its surface!", "The table hits you with its edge!"};
+        int randomNum = rand() % possibleAttacks.size();
+        if(randomNum == 0) {
+            cout << possibleAttacks[randomNum] << endl;
+            wait(1000);
+            int damage = 5;
+            health -= damage;
+            cout << "You take " << damage << " damage." << endl;
+            wait(1000);
+        } else if(randomNum == 1) {
+            cout << possibleAttacks[randomNum] << endl;
+            wait(1000);
+            int damage = 10;
+            health -= damage;
+            cout << "You take " << damage << " damage." << endl;
+            wait(1000);
+        } else if(randomNum == 2) {
+            cout << possibleAttacks[randomNum] << endl;
+            wait(1000);
+            int damage = 15;
+            health -= damage;
+            cout << "You take " << damage << " damage." << endl;
+            wait(1000);
+        }
+
+
+
     } else if (encounter == "Spider man") {
+        cout << "The Spider-Man hits you with its web!" << endl;
+        wait(1000);
+        int damage = 15;
+        health -= damage;
+        cout << "You take " << damage << " damage." << endl;
+        wait(1000);
+    } else if (encounter == "The Green Man") {
+        cout << "The Green Man hits you with its club!" << endl;
+        wait(1000);
+        int damage = 20;
+        health -= damage;
+        cout << "You take " << damage << " damage." << endl;
+        wait(1000);
+    } else if (encounter == "Knife") {
+        cout << "The Knife hits you with its blade!" << endl;
+        wait(1000);
+        int damage = 25;
+        health -= damage;
+        cout << "You take " << damage << " damage." << endl;
+        wait(1000);
+    }
+}
+
+vector<int> introductions(string& encounter) {
+    vector<int> stats;
+    if(encounter == "Table") {
+        stats = {100, 5, 20, 0};
+        cout << "the ordinary table wasn't ordinary because table was not table" << endl;
+        wait(2000);
+        cout << "table" << endl;
+        wait(1500);
+    } else if (encounter == "Spider man") {
+        stats = {100, 5, 20 , 0};
         cout << "Spider-Man hates you." << endl;
         wait(500);
         cout << "Why?" << endl;
@@ -95,6 +189,7 @@ void introductions(string& encounter) {
         cout << "Idk" << endl;
         wait(1000);
     } else if (encounter == "The Green Man") {
+        stats = {100, 5, 20, 0};
         cout << "The Green Man doesn't like you." << endl;
         wait(1000);
         cout << "You are a purple person." << endl;
@@ -102,6 +197,7 @@ void introductions(string& encounter) {
         cout << "Kratos verde" << endl;
         wait(1000);
     } else if (encounter == "Knife") {
+        stats = {100, 5, 20, 0};
         cout << "The tetanus knife is back again" << endl;
         wait(1000);
         cout << "yes its the one that makes you explode" << endl;
@@ -113,9 +209,17 @@ void introductions(string& encounter) {
     } else {
         cout << "Error: Encounter not recognized." << endl;
     }
+    return stats;
 }
 
-void printStats(int& maxHealth, int& health, int& attack, int& defense, vector<string>& effects) {
+void printStats(int& maxHealth, int& health, int& attack, int& defense, vector<string>& effects, vector<int>& enemyStats) {
+    cout << "Enemy stats: " << endl;
+    wait(250);
+    cout << "Health: " << enemyStats[0] << endl;
+    wait(250);
+    cout << "Defense: " << enemyStats[2] << endl;
+    wait(500);
+    cout << "\n-----------------------------\n" << endl;
     cout << "Player stats: " << endl;
     wait(250);
     cout << "Health: " << health << "/" << maxHealth << endl;
@@ -131,9 +235,9 @@ void printStats(int& maxHealth, int& health, int& attack, int& defense, vector<s
             wait(250);
         }
     }
-    wait(1500);
+    wait(1250);
 }
 
 void wait(int milliseconds) {
-    //this_thread::sleep_for(chrono::milliseconds(milliseconds));
+    this_thread::sleep_for(chrono::milliseconds(milliseconds));
 }
